e34bc14ce6b59a1fcea2fd96f5ac700b7ee74952
Git-hit <ayanali8178@gmail.com>
2025-06-16 06:21:43 +0530

    Added debugging for filepath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost', 'https://api.dailytrendnews.in', 'api.dailytrendnews.in'],
+    domains: ['localhost', 'https://dailytrendnews.in', 'dailytrendnews.in'],
   },
 };
 
